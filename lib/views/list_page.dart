e170773228ca90bd441f6a66cf23c75c54a6deb7
hirosphere-ja <51845520+hirosphere-ja@users.noreply.github.com>
2022-10-02 15:17:43 +0900

    メモ詳細画面を作成
    Cardを使用してメモ詳細画面を仮作成

--- lib/views/list_page.dart
+++ lib/views/list_page.dart
@@ -9,8 +9,71 @@ class ListPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('ListPage'),
       ),
-      body: const Center(
-        child: Text('ListPage'),
+      body: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            ListTile(
+              title: Column(
+                children: [
+                  const Text('銘柄名'),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('(証券コード)'),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('市場'),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(4.0),
+                    child: Text('メモ'),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text('登録日時：createdAt'),
+                Text('更新日時：updatedAt'),
+              ],
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    shape: const StadiumBorder(),
+                  ),
+                  icon: const Icon(Icons.edit),
+                  label: const Text('編集'),
+                  onPressed: () {},
+                ),
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    shape: const StadiumBorder(),
+                  ),
+                  icon: const Icon(Icons.delete),
+                  label: const Text('削除'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
